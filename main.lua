1aa8e564178a95fe4eb4cf3c12b599b329c26325
jneda <julien.dane@gmail.com>
2022-07-07 18:06:24 +0200

    Formatted main.lua

--- main.lua
+++ main.lua
@@ -1,29 +1,29 @@
 --main functions
 
 function _init()
-	screen_width=128
-	screen_height=128
+ screen_width=128
+ screen_height=128
 
-	tile_size=8
-	
-	map_width=screen_width/tile_size
-	map_height=screen_height/tile_size
+ tile_size=8
+ 
+ map_width=screen_width/tile_size
+ map_height=screen_height/tile_size
 
  room_max_size=7
  room_min_size=3
  max_rooms=10
-	
-	char_offset_x=2
-	char_offset_y=1
-	
-	player_x=flr(map_width/2)
-	player_y=flr(map_height/2)
-	
-	player=make_entity(player_x,player_y,"@",7)
-	npc=make_entity(player_x-5,player_y,"@",10)
-	entities={npc,player}
-	
-	gamemap=generate_dungeon(
+ 
+ char_offset_x=2
+ char_offset_y=1
+ 
+ player_x=flr(map_width/2)
+ player_y=flr(map_height/2)
+ 
+ player=make_entity(player_x,player_y,"@",7)
+ npc=make_entity(player_x-5,player_y,"@",10)
+ entities={npc,player}
+ 
+ gamemap=generate_dungeon(
   max_rooms,
   room_min_size,
   room_max_size,
@@ -32,41 +32,41 @@ function _init()
   player
  )
 
-	debug_msg=""
+ debug_msg=""
 end
 
 function _update()
-	action=handle_input()
-	if action then
-		if action.type=="move" then
-		 --invoke method
-		 action:perform(gamemap,player)
+ action=handle_input()
+ if action then
+  if action.type=="move" then
+   --invoke method
+   action:perform(gamemap,player)
   end
  end
 end
 
 function _draw()
-	cls()
-	--[[
-	--debug lines
-	for i=0,15 do
-	 --horizontal
-	 line(7+i*8,0,7+i*8,127)
-	 --vertical
-	 line(0,7+i*8,127,7+i*8)
-	end
-	--]]
-	
-	gamemap:draw()
-	
-	for entity in all(entities) do
-		print(
-		 entity.char,
-		 entity.x*tile_size+char_offset_x,
-		 entity.y*tile_size+char_offset_y,
-		 entity.col
-		)
-	end
+ cls()
+ --[[
+ --debug lines
+ for i=0,15 do
+  --horizontal
+  line(7+i*8,0,7+i*8,127)
+  --vertical
+  line(0,7+i*8,127,7+i*8)
+ end
+ --]]
+ 
+ gamemap:draw()
+ 
+ for entity in all(entities) do
+  print(
+   entity.char,
+   entity.x*tile_size+char_offset_x,
+   entity.y*tile_size+char_offset_y,
+   entity.col
+  )
+ end
 
-	print(debug_msg,0,0,3)
+ print(debug_msg,0,0,3)
 end
